40d744181d861ec72838fd1d2046db4ca05dc226
Seu Nome <adryanportella8@gmail.com>
2026-04-19 23:49:19 -0300

    feat: contracts models

---
+++ src/app/shared/models/contract.model.ts
@@ -0,0 +1,29 @@
+export interface IClient {
+  id: string;
+  name: string;
+  email: string;
+  phone: string;
+  cnpjf: string;
+}
+
+export interface IInstallment {
+  id: string;
+  contractId: string;
+  number: number;
+  dueDate: Date;
+  value: number;
+  paid: boolean;
+  paidAt: Date;
+}
+
+export interface IContract {
+  id: string;
+  client: IClient;
+  description: string;
+  totalValue: number;
+  targetValue: number;
+  startDate: Date;
+  monthsDuration: number;
+  installments: IInstallment[];
+  createAt: Date;
+}
