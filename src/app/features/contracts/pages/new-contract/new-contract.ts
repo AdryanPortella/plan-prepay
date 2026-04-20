c22acc2ed81649d8a14d7fb006831da28def03a4
Seu Nome <adryanportella8@gmail.com>
2026-04-20 00:00:54 -0300

    chore: add ng-zorro-antd

---
+++ src/app/features/contracts/pages/new-contract/new-contract.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-contract',
+  imports: [],
+  templateUrl: './new-contract.html',
+  styleUrl: './new-contract.scss',
+})
+export class NewContract {
+
+}
